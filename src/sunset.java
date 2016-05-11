7060f6a402fcd29e9a8c9d3b1e121ab213caae78
allison.white <allison.white@MU-C1-09.mullen.pvt.k12.co.us>
2016-05-11 09:59:30 -0600

    Graphics2

--- src/sunset.java
+++ src/sunset.java
@@ -21,29 +21,72 @@ public class sunset extends Canvas
 			}
 		public void paint(Graphics graphics)
 		{
-			 graphics.setColor(Color.GRAY);
+			graphics.setColor(new Color(139,90,43));
+		     graphics.fillRect(50, 100, 40, 300);
+		     graphics.setColor(new Color(102,205,0));
+		     graphics.fillOval(50, 50, 50, 50);
+			graphics.setColor(new Color(210,180,140));
 		     graphics.fillRect(0, 250, 500, 300);
-		     graphics.setColor(Color.cyan);
+		     graphics.setColor(new Color(0,151,190));
 		     graphics.fillRect(0,0, 1000, 250);
+		     
+		     
 		   
-		     for (int i = 0; i < 300; i++) 
+		     for (int i = 50; i < 300; i++) 
 		    	 {
-				        graphics.setColor(Color.yellow);
+		    		 graphics.setColor(new Color(139,90,43));//treebranch
+				     graphics.fillRect(50, 100, 40, 300);
+				     graphics.setColor(new Color(102,205,0));//treeleaves
+				     graphics.fillOval(50, 50, 50, 50); 
+				     
+		    		 graphics.setColor(Color.yellow);//sun
 				        graphics.fillOval(200, i, 50, 50);
+				        graphics.setColor(new Color(210,180,140));//sand
+				        graphics.fillRect(0,250,500,300);
+				        
 				        
 				        delay();
+				      
+				        graphics.setColor(new Color(0,151,190));//sky
+					     graphics.fillRect(0,0, 1000, 250);
 				        
-				        graphics.setColor(Color.cyan);
-				        graphics.fillOval(200, i, 50, 50);
-				       if ()
+				    
+					     graphics.setColor(new Color(139,90,43));//treebranch
+				     graphics.fillRect(50, 100, 40, 300);
+					     graphics.setColor(new Color(102,205,0));//treeleaves
+				     graphics.fillOval(50, 50, 50, 50);
 			        	}
+		     for (int j =300; j >50; j--) 
+		    	 {
+		    		 graphics.setColor(new Color(139,90,43));//treebranch
+				     graphics.fillRect(50, 100, 40, 300);
+				     graphics.setColor(new Color(102,205,0));//treeleaves
+				     graphics.fillOval(50, 50, 50, 50);   
+		    		 graphics.setColor(Color.GRAY);//moon
+				        graphics.fillOval(200, j, 50, 50);
+				       
+				        graphics.setColor(new Color(210,180,140));//sand
+				        graphics.fillRect(0,250,500,300);
+				        delay();
+				      
+				        graphics.setColor(new Color(210,180,140));//sand
+				        graphics.fillRect(0,250,500,300);
+				        graphics.setColor(new Color(0,151,190));//sky
+					     graphics.fillRect(0,0, 1000, 250);
+				        graphics.setColor(new Color(139,90,43));//treebranch
+					     graphics.fillRect(50, 100, 40, 300);
+						     graphics.setColor(new Color(102,205,0));//treeleaves
+					     graphics.fillOval(50, 50, 50, 50);
+				        
+				         
+		    	 }
 		}
 		private void delay()
 			{
 					{
 		    	        try
 		    					{
-		    					Thread.sleep(50);
+		    					Thread.sleep(60);
 		    					} catch (InterruptedException e)
 		    					{
 		    					e.printStackTrace();
